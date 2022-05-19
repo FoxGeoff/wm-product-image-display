7d18a51c6742bef413838c4b624b252d74976a09
Geoff Fox <fox.geoffrey@gmail.com>
2022-05-19 16:48:36 -0700

    update server.js (ref WingMan)

--- server.js
+++ server.js
@@ -1,31 +1,34 @@
-const express = require('express');
-const db = require('./server/models/db');
+const express = require("express");
+const db = require("./server/models/db");
 const app = express();
 const port = 3000;
-const seed = require('./server/models/seed/seed-db');
+const seed = require("./server/models/seed/seed-db");
 
-// setup the Express middlware
-require('./server/middleware/middleware')(app);
+// Setup the Express middleware
+require("./server/middleware/middleware")(app);
 
-// setup the api
-require('./server/api')(app);
+// Setup the api
+require("./server/api")(app);
 
-// Uncomment below to Seed Database First
-// db.sequelize.sync(
-//   { force: true }
-//   )
-//   .then(() => {
-//     seed.insert();
-//   })
-//   .then(() => {
-//       app.listen(port, () => {
-//           console.log('running server on port ' + port);
-//     })
-// });
-
-// Connect to DB then run server after seeding
-db.sequelize.sync().then(() => {
-  app.listen(port, () => {
-    console.log('running server on port ' + port);
+// Connect to Db then start the server
+db.sequelize
+  .sync({
+    logging: console.log,
+    force: true, // Set to true for Db updates
   })
+  /* No seed data  */
+  .then(() => {
+    seed.insert();
+  })
+  .then(() => {
+    app.listen(port, () => {
+      console.log("running server on port " + port);
+    });
+  });
+
+// Start the server - Debug code
+/*
+app.listen(port, () => {
+  console.log(`running server on port ${port} `);
 });
+*/
